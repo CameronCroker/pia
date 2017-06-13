c00a1635156b7ccbd474a1abc6a80adf59fb5e53
CAMERON-PC\cameron <cameron.croker@students.mq.edu.au>
2017-06-13 23:51:00 +1000

    changed db location

--- Hornsby-PIA/Interface1.cs
+++ Hornsby-PIA/Interface1.cs
@@ -23,14 +23,10 @@ namespace Hornsby_PIA
         static string Name;
         static string View;
 
-
-        static private string dataSource = "data source="+GetSolutionDirectory()+"\\SQLiteDB.db";
+        
+        static private string dataSource = "data source="+ AppDomain.CurrentDomain.BaseDirectory + "\\PIA" + "\\SQLiteDB.db";
         static public SQLiteConnection conn = new SQLiteConnection(dataSource);
         
-        static public string GetSolutionDirectory()
-        {
-            return Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())));
-        }
         public void connect()
         {
             try
